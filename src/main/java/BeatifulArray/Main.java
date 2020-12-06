90a69c0e319cb67471aceed4c7fca3e934860265
Denis <mai.borisov.denis@gmail.com>
2020-12-06 19:40:32 +0300

    message

--- src/main/java/BeatifulArray/Main.java
+++ src/main/java/BeatifulArray/Main.java
@@ -1,5 +1,31 @@
 package BeatifulArray;
 
+import javax.jnlp.IntegrationService;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
+    public static ArrayList<Integer> beautifulArray(int n){
+        int value = 0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        while (numbers.size() < n) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int i : numbers) {
+                value = i * 2 - 1;
+                if (value <= n) {
+                    temp.add(value);
+                }
+            }
+            for (int i : numbers) {
+                int res = i * 2;
+                if (res <= n) {
+                    temp.add(i * 2);
+                }
+            }
+            numbers = temp;
+        }
+        return numbers;
+    }
 }
--- src/main/java/hometask1/MinPatcher.java
+++ src/main/java/hometask1/MinPatcher.java
@@ -3,12 +3,12 @@ package hometask1;
 public class MinPatcher {
     private int i;
     private int result;
-    private int edit = 1;
+    private int edit = 1;//минимальная сумма среди элементов [0, n]
         public int minPatches(int[] nums, int n){
         while(edit <= n){
-            if (i < nums.length && nums[i] <= edit){
-                edit += nums[i + 1];
-            } else {
+            if (i < nums.length && nums[i] <= edit){//если есть такое число которое меньше edit в последовательности то мыдолжны добавить его к сумме чтобы выстроить последовательность с максимальным количеством элементов
+                edit += nums[i++];
+            } else {//если такго числа нет, то мы должны его добавить и поаторить алгоритм
                 edit += edit;
                 result++;
             }
