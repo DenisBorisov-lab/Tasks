26722b1c1db27733bf65984242702da34bc62fae
Denis <mai.borisov.denis@gmail.com>
2020-12-14 11:50:20 +0300

    'fics

--- src/main/java/Test/Test.java
+++
@@ -1,9 +0,0 @@
-package Test;
-
-public class Test {
-    public static void main(String[] args) {
-        String number = "abcabcabc";
-
-        System.out.println(number.contains("ab"));
-    }
-}
